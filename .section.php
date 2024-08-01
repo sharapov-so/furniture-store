4df21847a528f58d707dce1a91e4b316dd711cb2
Sharapov-SO <semka.3run@gmail.com>
2024-08-01 09:49:43 +0300

    init Bitrix

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Мебельный магазин',
+	'description' => 'Description',
+	'keywords' => 'Keywords',
+	'robots' => 'index, follow'
+);
+?>
