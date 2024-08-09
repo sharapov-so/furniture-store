c08027ab5ebb605460164e188478b737a4a71232
Sharapov-SO <semka.3run@gmail.com>
2024-08-09 16:30:54 +0500

    The template has been configured. The components for the template have been configured. Added 2 custom components. Added missing pages on the site.

--- local/templates/test/components/bitrix/news/reviews/bitrix/news.index/.default/template.php
+++ local/templates/test/components/bitrix/news/reviews/bitrix/news.index/.default/template.php
@@ -11,20 +11,16 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
-
-$arViewModeList = $arResult['VIEW_MODE_LIST'];
-
-
-
 ?>
-    <ul>
-<?php foreach ($arResult["SECTIONS"] as $arItem): ?>
-
-    <li>
-        <a href="/products/section.php?SECTION=<?= $arItem["ID"] ?>"><?= $arItem["NAME"] ?></a>
-    </li>
-<?php endforeach; ?>
-    </ul>
-
-
-
+<div class="news-index">
+<?foreach($arResult["IBLOCKS"] as $arIBlock):?>
+	<?if(count($arIBlock["ITEMS"])>0):?>
+		<b><?=$arIBlock["NAME"]?></b>
+		<ul>
+		<?foreach($arIBlock["ITEMS"] as $arItem):?>
+			<li><a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><?=$arItem["NAME"]?></a></li>
+		<?endforeach;?>
+		</ul>
+	<?endif?>
+<?endforeach;?>
+</div>
