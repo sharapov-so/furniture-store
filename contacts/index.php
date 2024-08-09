c6e303bf8072eda78832657597427da14f5c7ca6
Sharapov-SO <semka.3run@gmail.com>
2024-08-09 16:51:49 +0500

    Feedback form added

--- contacts/index.php
+++ contacts/index.php
@@ -2,8 +2,16 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetPageProperty("TITLE", "Контакты");
 $APPLICATION->SetTitle("Контакты");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>С нами вы можете связаться через данную форму:<br>
+ <br>
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback",
+	"",
+	Array(
+		"EMAIL_TO" => "semka.3run@gmail.com",
+		"EVENT_MESSAGE_ID" => array("7"),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array("NAME","EMAIL","MESSAGE"),
+		"USE_CAPTCHA" => "Y"
+	)
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
